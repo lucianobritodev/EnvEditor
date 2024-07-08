9bf7b8378a2a4597054484f55a819525b793d89c
luciano <F675089@corp.caixa.gov.br>
2024-07-08 19:26:54 -0300

    feat: Modal Sobre - Preenchimento de textarea pelo controlador para possibilitar a exibição de versão de forma dinâmica

--- src/main/java/br/com/lucianobrito/enveditor/controllers/SobreController.java
+++ src/main/java/br/com/lucianobrito/enveditor/controllers/SobreController.java
@@ -1,11 +1,25 @@
 package br.com.lucianobrito.enveditor.controllers;
 
+import br.com.lucianobrito.enveditor.service.EnvFilesService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
-public class SobreController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+public class SobreController implements Initializable {
+
+    private final Logger LOGGER = Logger.getLogger(SobreController.class.getName());
+
+    private String version;
+
+    @FXML
+    private TextArea txtArea;
 
     @FXML
     protected void btnFechar(ActionEvent event) {
@@ -14,4 +28,27 @@ public class SobreController {
         stage.close();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            version = EnvFilesService.getInstance().getVersionApp();
+            txtArea.setText(getDescription());
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+        }
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    private String getDescription() {
+        return new StringBuilder().append("Aplicativo opensource para edição de variáveis de\n")
+                .append("ambiente em distribuições Linux.\n\n")
+                .append("Versão: " + version + "\n")
+                .append("Autor: Luciano Brito\n")
+                .append("Licença: GPL-2.0\n")
+                .append("Repositório: \nhttps://github.com/lucianobritodev/EnvEditor")
+                .toString();
+    }
 }
